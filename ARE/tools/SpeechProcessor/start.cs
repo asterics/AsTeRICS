af248c280f0a00954533eda98aa68125d80fd3bd
Chris Veigl <veigl@technikum-wien.at>
2014-03-17 17:37:15 +0100

    added start module

---
+++ ARE/tools/SpeechProcessor/start.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsynchronousRecognition
+{
+    class StartSpeechServer
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Recog.speechPlatformMain();
+                Environment.Exit(0);
+            }
+            catch (Exception e) { Console.WriteLine("\nException:" + e.Message); Environment.Exit(1); }
+        }
+    }
+
+    
+}
