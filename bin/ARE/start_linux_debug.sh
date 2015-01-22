d3b4fd2bba479d85c1c90b193982ddf5e3a60dc4
deinhofer <deinhofe@technikum-wien.at>
2015-01-22 14:27:06 +0100

    renamed start_linux.sh and start_linux_debug.sh to start.sh and start_debug.sh

--- bin/ARE/start_linux_debug.sh
+++
@@ -1,4 +0,0 @@
-echo AsTeRICS ARE Version 2.2
-echo Starting AsTeRICS Runtime Environment ...
-#-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -Dorg.osgi.framework.bootdelegation=* -DAnsi=true
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=1044 -Dorg.osgi.framework.bootdelegation=* -DAnsi=true -Djava.util.logging.config.file=logging.properties -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration profile -console
