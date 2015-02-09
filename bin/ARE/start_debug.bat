5bab32e90958a5ddc142f50d1d54037c868c7295
Martin Deinhofer <martin.deinhofer@technikum-wien.at>
2015-02-09 12:52:32 +0100

    Fixed startup of websocket demo: --webservice flag and auto startup of WebSocket_test.acs Startup of optional webservice is now done using the optional service file services_websocketdemo.ini, so no copying of backup files necessary any more. This also fixes #63
    TODO: the start.bat and start_debug.bat scripts should be unified --> now there is lot of duplicate code in both files

--- bin/ARE/start_debug.bat
+++ bin/ARE/start_debug.bat
@@ -8,31 +8,21 @@
 CLS
 
 SET PROFILE_PATH="profile"
+SET ARE_OPTIONAL_SERVICES_INI=""
 
 @REM Check commandline parameter
 @IF "%1" == "--webservice" (
 	@    echo "--webservice selected"
 	@    SET AUTOSTART_MODEL=WebSocket_test.acs
-	copy   %PROFILE_PATH%\\loader.ini %PROFILE_PATH%\\loader.ini.backup
-	copy   %PROFILE_PATH%\\services.ini %PROFILE_PATH%\\services.ini.backup
-	copy   %PROFILE_PATH%\\loader_websocketdemo.ini %PROFILE_PATH%\\loader.ini
-	copy   %PROFILE_PATH%\\services_websocketdemo.ini %PROFILE_PATH%\\services.ini
+	@    SET ARE_OPTIONAL_SERVICES_INI="services_websocketdemo.ini"
 
 ) ELSE (
-
 	@     SET AUTOSTART_MODEL=%1
-
-	IF EXIST "%PROFILE_PATH%\\loader.ini.backup" (
-		copy   %PROFILE_PATH%\\loader.ini.backup %PROFILE_PATH%\\loader.ini
-	)
-	
-	IF EXIST "%PROFILE_PATH%\\services.ini.backup" (
-		copy   %PROFILE_PATH%\\services.ini.backup %PROFILE_PATH%\\services.ini
-	)
 )
 
 @echo "Using profile path: %PROFILE_PATH%"
 @echo "Using autostart model: %AUTOSTART_MODEL%"
+@echo "Using optional services: %ARE_OPTIONAL_SERVICES_INI%"
 
 set JAVA_BIN="java"
 
@@ -68,7 +58,7 @@ rd /s/q %PROFILE_PATH%\org.eclipse.osgi
 
 ECHO Starting AsTeRICS Runtime Environment with Debug output ...
 ECHO error_level:FINE>.logger
-%JAVA_BIN% -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -Dosgi.clean=true -Dorg.osgi.framework.bootdelegation=* -Dorg.osgi.framework.system.packages.extra=sun.misc -DAnsi=true -Djava.util.logging.config.file=logging.properties -Deu.asterics.ARE.startModel=%AUTOSTART_MODEL% -Deu.asterics.ARE.ServicesFiles="services.ini;services-windows.ini" -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration %PROFILE_PATH% -console
+%JAVA_BIN% -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -Dosgi.clean=true -Dorg.osgi.framework.bootdelegation=* -Dorg.osgi.framework.system.packages.extra=sun.misc -DAnsi=true -Djava.util.logging.config.file=logging.properties -Deu.asterics.ARE.startModel=%AUTOSTART_MODEL% -Deu.asterics.ARE.ServicesFiles="services.ini;services-windows.ini;%ARE_OPTIONAL_SERVICES_INI%" -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration %PROFILE_PATH% -console
 
 REM If you want to enable Java Mission Control and Flight Recording flags use the line below
 REM %JAVA_BIN% -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -Dorg.osgi.framework.bootdelegation=* -DAnsi=true -Djava.util.logging.config.file=logging.properties  -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration %PROFILE_PATH% -console
