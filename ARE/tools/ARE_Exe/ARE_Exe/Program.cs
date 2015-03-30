60ce21d5edeca34d82cac11528e034fdb454d009
eldufdo <david.thaller@servus.at>
2015-03-30 15:19:18 +0200

    changed ARE.exe to a C# program because the arguments list and cwd attributes where easier to implement there

---
+++ ARE/tools/ARE_Exe/ARE_Exe/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace ARE_Exe
+{
+    class Program
+    {
+        static void ExecuteCommand(string command, string args)
+        {
+            int exitCode;
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo(command);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+            processInfo.WorkingDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            processInfo.Arguments = args;
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+
+            // *** Read the streams ***
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            exitCode = process.ExitCode;
+
+            Console.WriteLine("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+            Console.WriteLine("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+            Console.WriteLine("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
+            process.Close();
+        }
+
+        static string StringArrayToString(string[] array)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (string value in array)
+            {
+                builder.Append(value);
+                builder.Append(' ');
+            }
+            return builder.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            ExecuteCommand("start.bat", StringArrayToString(args));
+        }
+    }
+}
--- ARE/tools/ARE_executable/ARE_executable/ReadMe.txt
+++
@@ -1,1 +0,0 @@
-﻿this executable is used to start the start.bat file in the ARE bin folder
