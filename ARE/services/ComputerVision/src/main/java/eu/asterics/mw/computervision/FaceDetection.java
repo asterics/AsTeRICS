d961baf153f8090bf8569a239677bcf2bc84fc02
Martin Deinhofer <martin.deinhofer@technikum-wien.at>
2015-04-17 15:19:47 +0200

    fixed compile error in FaceDetection.java

--- ARE/services/ComputerVision/src/main/java/eu/asterics/mw/computervision/FaceDetection.java
+++ ARE/services/ComputerVision/src/main/java/eu/asterics/mw/computervision/FaceDetection.java
@@ -68,8 +68,9 @@ public class FaceDetection {
 		int total = faces.total();
 
 		if(total > 0) {
-			/*
+			
 			CvRect faceRect=new CvRect(cvGetSeqElem(faces, 0));
+			/*
 			opencv_core.cvReleaseImage(grayImage);
 			grayImage=null;
 			 */
