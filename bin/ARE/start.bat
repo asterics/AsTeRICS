251cfc2abce2bb70cf95c486146d13f2adbfc271
Chris Veigl <veigl@technikum-wien.at>
2015-05-13 13:26:35 +0200

    updated start.bat (closing console window)

--- bin/ARE/start.bat
+++ bin/ARE/start.bat
@@ -1,11 +1,11 @@
 :Initialization
-@REM VERY IMPORTANT: The two lines ensure that the script is executed in the current directy (ARE), also when called
-@REM as administrator
+REM VERY IMPORTANT: The two lines ensure that the script is executed in the current directy (ARE), also when called
+REM as administrator
 @setlocal enableextensions
 @cd /d "%~dp0"
 
 CLS
-set JAVA_BIN="javaw"
+set JAVA_BIN=javaw
 
 if exist java\bin\java.exe (
 	set JAVA_BIN=java\bin\javaw.exe
@@ -20,24 +20,14 @@ IF EXIST asterics.ARE.jar GOTO Continue
 ECHO ARE jar files not found, please unzip ARE.zip or copy jar files into this folder !
 GOTO QuitError
 
-
-
 :Continue
-
 ECHO Starting AsTeRICS Runtime Environment ...
 ECHO error_level:WARNING>.logger
 
-REM use next two lines for starting without the shell window
-REM start javaw -Djava.util.logging.config.file=logging.properties -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration profile -console
-REM sleeper 5
-
-
-REM use the next line instead for starting with the shell window (or next line without splash screen)
-%JAVA_BIN% -splash:images/asterics_startup.png -Djava.util.logging.config.file=logging.properties -Dosgi.clean=true -Dorg.osgi.framework.bootdelegation=* -Deu.asterics.ARE.startModel=%1 -Deu.asterics.ARE.ServicesFiles="services.ini;services-windows.ini" -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration profile -console
+start %JAVA_BIN% -splash:images/asterics_startup.png -Djava.util.logging.config.file=logging.properties -Dosgi.clean=true -Dorg.osgi.framework.bootdelegation=* -Deu.asterics.ARE.startModel=%1 -Deu.asterics.ARE.ServicesFiles="services.ini;services-windows.ini" -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration profile -console
 REM %JAVA_BIN% -Djava.util.logging.config.file=logging.properties -Dorg.osgi.framework.bootdelegation=* -Deu.asterics.ARE.startModel=%1 -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration profile -console
-
 GOTO Quit
 
 :QuitError
-
+pause
 :Quit
