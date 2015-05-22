b58567d249b3c9c5c2334e25a7a2cf82ead94a34
Martin Deinhofer <martin.deinhofer@technikum-wien.at>
2015-05-22 13:47:30 +0200

    Create CvMemStorage in ctor of FaceDetection, otherwise opencv crashes and leads to big memory leak.

--- ARE/services/ComputerVision/src/main/java/eu/asterics/mw/computervision/FaceDetection.java
+++ ARE/services/ComputerVision/src/main/java/eu/asterics/mw/computervision/FaceDetection.java
@@ -5,8 +5,14 @@ import java.net.URL;
 
 import org.bytedeco.javacv.*;
 import org.bytedeco.javacpp.*;
+import org.bytedeco.javacpp.opencv_core.CvMemStorage;
+import org.bytedeco.javacpp.opencv_core.CvRect;
+import org.bytedeco.javacpp.opencv_core.CvSeq;
+import org.bytedeco.javacpp.opencv_core.IplImage;
+import org.bytedeco.javacpp.opencv_objdetect.CvHaarClassifierCascade;
 
 import eu.asterics.mw.services.AstericsErrorHandling;
+import static org.bytedeco.javacpp.helper.opencv_objdetect.cvHaarDetectObjects;
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 import static org.bytedeco.javacpp.opencv_calib3d.*;
@@ -22,6 +28,11 @@ import static org.bytedeco.javacpp.opencv_objdetect.*;
 public class FaceDetection {
 	String classifierName = "data/service.computervision/haarcascade_frontalface_alt.xml";
 	CvHaarClassifierCascade classifier=null;
+	// Objects allocated with a create*() or clone() factory method are automatically released
+	// by the garbage collector, but may still be explicitly released by calling release().
+	// You shall NOT call cvReleaseImage(), cvReleaseMemStorage(), etc. on objects allocated this way.
+	CvMemStorage storage;
+
 
 	public FaceDetection() {
 		super();
@@ -29,15 +40,27 @@ public class FaceDetection {
 		Loader.load(opencv_objdetect.class);
 
 		// We can "cast" Pointer objects by instantiating a new object of the desired class.
+		System.out.println("Loading haarcascade classifier");
 		classifier = new CvHaarClassifierCascade(cvLoad(classifierName));
 		if (classifier.isNull()) {        	
-			AstericsErrorHandling.instance.getLogger().warning("Error loading classifier file \"" + classifierName + "\".");
+			System.err.println("Error loading classifier file \"" + classifierName + "\".");
 		}              		
+		
+		// Objects allocated with a create*() or clone() factory method are automatically released
+		// by the garbage collector, but may still be explicitly released by calling release().
+		// You shall NOT call cvReleaseImage(), cvReleaseMemStorage(), etc. on objects allocated this way.
+		storage = CvMemStorage.create();
 	}
 
-	public CvRect detectFace(IplImage grabbedImage) throws Exception {
-
-
+	public CvRect detectFace(IplImage grayImage) throws Exception {
+		// Let's try to detect some faces! but we need a grayscale image...
+		/*
+		int width  = grabbedImage.width();
+		int height = grabbedImage.height();
+		IplImage grayImage    = IplImage.create(width, height, IPL_DEPTH_8U, 1);        
+		cvCvtColor(grabbedImage, grayImage, CV_BGR2GRAY);
+*/
+		
 		// FAQ about IplImage:
 		// - For custom raw processing of data, getByteBuffer() returns an NIO direct
 		//   buffer wrapped around the memory pointed by imageData, and under Android we can
@@ -46,37 +69,37 @@ public class FaceDetection {
 		// - The createFrom() factory method can construct an IplImage from a BufferedImage.
 		// - There are also a few copy*() methods for BufferedImage<->IplImage data transfers.
 		//IplImage grabbedImage = grabber.grab();
-		int width  = grabbedImage.width();
-		int height = grabbedImage.height();
-		IplImage grayImage    = IplImage.create(width, height, IPL_DEPTH_8U, 1);        
-
-		// Objects allocated with a create*() or clone() factory method are automatically released
-		// by the garbage collector, but may still be explicitly released by calling release().
-		// You shall NOT call cvReleaseImage(), cvReleaseMemStorage(), etc. on objects allocated this way.
-		CvMemStorage storage = CvMemStorage.create();
-
 
 		cvClearMemStorage(storage);
-
-		// Let's try to detect some faces! but we need a grayscale image...
-		cvCvtColor(grabbedImage, grayImage, CV_BGR2GRAY);
-
+		//storage.release();
 
 		CvSeq faces = cvHaarDetectObjects(grayImage, classifier, storage,
 				1.1, 1, CV_HAAR_DO_ROUGH_SEARCH | CV_HAAR_FIND_BIGGEST_OBJECT);
 
 		int total = faces.total();
 
+		//grabbedImage.release();
+		/*
+		grayImage.release();
+		grayImage=null;
+		*/
 		if(total > 0) {
 			
 			CvRect faceRect=new CvRect(cvGetSeqElem(faces, 0));
-			/*
-			opencv_core.cvReleaseImage(grayImage);
-			grayImage=null;
-			 */
+			
+			//storage.release();
 			return faceRect;
 		} 
+		//storage.release();
 		return null;
+	}
+	
+	public IplImage convertToGrayScaleIplImage(IplImage orig) {
+		int width  = orig.width();
+		int height = orig.height();
+		IplImage grayImage    = IplImage.create(width, height, IPL_DEPTH_8U, 1);
+		cvCvtColor(orig, grayImage, CV_BGR2GRAY);
+		return grayImage;
 	}
 
 	public void drawFaceRect(CvRect faceRect, IplImage image) {
