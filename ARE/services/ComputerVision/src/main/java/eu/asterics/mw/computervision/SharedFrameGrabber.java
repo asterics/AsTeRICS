39ba1b38a387e528e5f181a106bd5caaf49be822
deinhofer <deinhofe@technikum-wien.at>
2015-05-28 16:34:23 +0200

    added sanity check for OpenCV on Linux to not crash if camera device does not exist added automatic detection of available framegrabbers per platform added creation of default framegrabber added support for deviceFile and devicePath

--- ARE/services/ComputerVision/src/main/java/eu/asterics/mw/computervision/SharedFrameGrabber.java
+++ ARE/services/ComputerVision/src/main/java/eu/asterics/mw/computervision/SharedFrameGrabber.java
@@ -1,20 +1,20 @@
 package eu.asterics.mw.computervision;
 
-import java.lang.management.ThreadMXBean;
-import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
-import org.bytedeco.javacpp.opencv_core;
-import org.bytedeco.javacpp.opencv_core.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bytedeco.javacpp.opencv_core.IplImage;
 import org.bytedeco.javacv.CameraDevice;
 import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.FrameGrabber.Exception;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
 import org.bytedeco.javacv.VideoInputFrameGrabber;
 
 import eu.asterics.mw.services.AstericsErrorHandling;
+import eu.asterics.mw.utils.OSUtils;
 
 /**
  * Contains code to unify/simplify enumeration/initialization/opening/closing of camera devices and frame grabbing. 
@@ -25,6 +25,14 @@ import eu.asterics.mw.services.AstericsErrorHandling;
  */
 public class SharedFrameGrabber {
 	//timeout for grabber thread to die in ms.
+	public static final String DEFAULT_GRABBER_KEY="Default";
+	public static final String OPENCV_GRABBER_KEY="OpenCV";
+	public static final String VIDEOINPUT_GRABBER_KEY="VideoInput";
+	public static final String OPENKINECT_GRABBER_KEY="OpenKinect";
+	public static final String IPCAMERA_GRABBER_KEY="IPCamera";
+	public static final String PS3Eye_GRABBER_KEY="PS3Eye";
+	public static final String FFMPEG_GRABBER_KEY="FFmpeg";
+	
 	private static final long GRABBER_STOP_TIMEOUT = 10000;
 	public static SharedFrameGrabber instance=new SharedFrameGrabber();
 	
@@ -32,69 +40,109 @@ public class SharedFrameGrabber {
 	private Map<String, List<GrabbedImageListener>> listeners=new HashMap<String, List<GrabbedImageListener>>(); 
 	private Map<String, GrabberThread> grabberThreads=new HashMap<String, GrabberThread>();
 	
-	private List<String> grabberList=new ArrayList<String>();
+	private List<String> grabberList=null;
+	private String[] defaultGrabberList=new String[]{VIDEOINPUT_GRABBER_KEY,OPENCV_GRABBER_KEY};
 	
 	public SharedFrameGrabber() {
-		grabberList.add("VideoInput");
-		grabberList.add("OpenCV");
-	}
-	private void init() throws Exception {
-		init(null,"0",320,240);
 	}
 	private void init(String grabberName, String deviceKey, int userWidth, int userHeight) throws Exception {
 		FrameGrabber grabber=null;
-		
+
+		System.out.println("Available grabber: "+getFrameGrabberList());
+		System.out.println("Default FrameGrabber: "+getDefaultFrameGrabberName());
+		System.out.println("FrameGrabber: "+grabberName);
+		System.out.println("DeviceKey: "+deviceKey);
+
 		if(device2FrameGrabber.containsKey(deviceKey)) {
 			AstericsErrorHandling.instance.getLogger().fine("Removing old FrameGrabber with key <"+deviceKey+">");
+			stopGrabbing(deviceKey);
 			FrameGrabber _grabber=device2FrameGrabber.get(deviceKey);
-			_grabber.stop();
 			_grabber.release();
 			device2FrameGrabber.remove(deviceKey);
 		}
-        // The available FrameGrabber classes include OpenCVFrameGrabber (opencv_highgui),
-        // DC1394FrameGrabber, FlyCaptureFrameGrabber, OpenKinectFrameGrabber,
-        // PS3EyeFrameGrabber, VideoInputFrameGrabber, and FFmpegFrameGrabber.
-             
-		int camIdx=Integer.parseInt(deviceKey);
-		if(grabberName == null) {
-			System.out.println("Creating default FrameGrabber");
-			//grabber=FrameGrabber.createDefault(camIdx);
-			try{
-				//default try to load videoInput
-				VideoInputFrameGrabber.tryLoad();
-				grabber=VideoInputFrameGrabber.createDefault(0);
-				
-				//grabber = FrameGrabber.create(FrameGrabber.list.get(5),0);
-			}catch(Throwable e) {
-				//if videoInput fails, try to load OpenCV
-				//grabber = FrameGrabber.create(FrameGrabber.list.get(6),0);	
-				OpenCVFrameGrabber.tryLoad();
-				grabber=OpenCVFrameGrabber.createDefault(0);
-			}
-		} else {
-	        System.out.println("List of grabbers (indices): "+grabberList);
-	        System.out.println("Using grabber: "+grabberName+", and camIdx: "+camIdx);
-	        grabber = FrameGrabber.create(grabberName,camIdx);
-	        
-	       
-	        //FFmpegFrameGrabber grabber =new FFmpegFrameGrabber("video=Integrated Camera");
-	        //grabber.setFormat("dshow");
-	        //FFmpegFrameGrabber grabber =new FFmpegFrameGrabber("0");
-	        //grabber.setFormat("vfwcap");
+		// The available FrameGrabber classes include OpenCVFrameGrabber (opencv_highgui),
+		// DC1394FrameGrabber, FlyCaptureFrameGrabber, OpenKinectFrameGrabber,
+		// PS3EyeFrameGrabber, VideoInputFrameGrabber, and FFmpegFrameGrabber.
+
+		if(grabberName == null || grabberName.equals(DEFAULT_GRABBER_KEY)) {			
+			grabberName=getDefaultFrameGrabberName();
+			System.out.println("Creating default FrameGrabber: "+grabberName);
 		}
-        
-        //FrameGrabber grabber = FrameGrabber.createDefault(frameGrabberIdx);
-        grabber.setImageHeight(userHeight);
-        grabber.setImageWidth(userWidth);
-        //grabber.setFrameRate(10);
-        
-		AstericsErrorHandling.instance.getLogger().fine("Adding FrameGrabber with key <"+deviceKey+">");
-        device2FrameGrabber.put(deviceKey, grabber);
-        //grabber.start();		       
+		
+		doSanityChecks(grabberName, deviceKey);
+
+		//System.out.println("Using grabber: "+grabberName+", and camIdx: "+camIdx);
+		//grabber = FrameGrabber.create(grabberName,camIdx);
+
+		CameraDevice.Settings devSet=new CameraDevice.SettingsImplementation();
+
+		try{
+			int camIdx=Integer.parseInt(deviceKey);
+			devSet.setDeviceNumber(camIdx);
+		}catch(NumberFormatException ne) {
+			if(new File(deviceKey).exists()) {
+				devSet.setDeviceFilename(deviceKey);
+			} else {
+				devSet.setDevicePath(deviceKey);
+			}				
+		}
+
+		devSet.setName(deviceKey);
+		devSet.setFrameGrabber(FrameGrabber.get(grabberName));
+		//devSet.setTimeout(20000);
+		devSet.setImageWidth(userWidth);
+		devSet.setImageHeight(userHeight);    
+		devSet.getDescription();
+		System.out.println(devSet.getDescription());
+
+		CameraDevice dev=new CameraDevice(devSet);  
+		System.out.println("CamDevice: "+dev);
+
+		grabber=dev.createFrameGrabber();
+
+		//FFmpegFrameGrabber grabber =new FFmpegFrameGrabber("video=Integrated Camera");
+		//grabber.setFormat("dshow");
+		//FFmpegFrameGrabber grabber =new FFmpegFrameGrabber("0");
+		//grabber.setFormat("vfwcap");
+
+		AstericsErrorHandling.instance.getLogger().fine("Adding FrameGrabber with key <"+deviceKey+">, grabber <"+grabber+">");
+		device2FrameGrabber.put(deviceKey, grabber);
 	}
 	
+	private void doSanityChecks(String grabberName, String deviceKey) throws Exception {
+		//Some dirty checks to prevent a crash of OpenCV on Linux if the device does not exist.
+		if(grabberName.equalsIgnoreCase("OpenCV") && (OSUtils.isUnix()||OSUtils.isMac())) {
+			try{
+				int camIdx=Integer.parseInt(deviceKey);
+				File vidFile=new File("/dev/video"+deviceKey);
+				if(!vidFile.exists()) {
+					throw new Exception("Cannot create FrameGrabber <"+grabberName+"> for device <"+deviceKey+">");
+				}
+			}catch(NumberFormatException e) {}
+		}
+	}
 	public List<String> getFrameGrabberList() {
+		if(grabberList==null) {
+			grabberList=new ArrayList<String>();
+			for(String grabberName : FrameGrabber.list) {
+				try {
+					Class<? extends FrameGrabber> c = FrameGrabber.get(grabberName);
+					c.getMethod("tryLoad").invoke(null);
+					grabberList.add(grabberName);
+				} catch (Throwable t) { }			
+			}
+		}
 		return grabberList;
+	}
+	
+	public String getDefaultFrameGrabberName() {
+		List<String> grabberList=getFrameGrabberList();
+		for(String grabberName : defaultGrabberList) {
+			if(grabberList.contains(grabberName)) {
+				return grabberName;
+			}
+		}
+		return "";
 	}
 
 	public FrameGrabber getFrameGrabber(String deviceKey, String grabberName, int width, int height) throws Exception {
@@ -207,17 +255,17 @@ public class SharedFrameGrabber {
 			FrameGrabber grabber;
 			List<GrabbedImageListener> deviceListeners=listeners.get(deviceKey);
 			try {
-				System.out.println("Start grabbing for devicekey <"+deviceKey+">");
 				grabber = getFrameGrabber(deviceKey);
-				grabber.start();
-				while(!stopGrabbing) {
-					IplImage image=grabber.grab();
-					notifyGrabbedImageListener(deviceListeners, image);
-					//opencv_core.cvReleaseImage(image);
-					//image=null;
+				System.out.println("Start grabbing for devicekey <"+deviceKey+">, grabber <"+grabber+">");
+				if(grabber!=null) {
+					grabber.start();
+					while(!stopGrabbing) {
+						IplImage image=grabber.grab();
+						notifyGrabbedImageListener(deviceListeners, image);
+					}
+					grabber.stop();
+					System.out.println("Grabbing stopped");
 				}
-				grabber.stop();
-				System.out.println("Grabbing stopped");
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
