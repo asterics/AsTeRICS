5ea33f63e8bbfabed02c65c13d7409c71ccfda79
Martin Deinhofer <martin.deinhofer@technikum-wien.at>
2015-06-11 12:41:42 +0200

    code of start_debug.bat moved to start.bat, which now also supports
    --webservice and autostart model

--- bin/ARE/start.bat
+++ bin/ARE/start.bat
@@ -1,14 +1,39 @@
 :Initialization
-REM VERY IMPORTANT: The two lines ensure that the script is executed in the current directy (ARE), also when called
-REM as administrator
+
+@REM VERY IMPORTANT: The two lines ensure that the script is executed in the current directy (ARE), also when called
+@REM as administrator
 @setlocal enableextensions
 @cd /d "%~dp0"
 
 CLS
-set JAVA_BIN=javaw
 
-if exist java\bin\java.exe (
-	set JAVA_BIN=java\bin\javaw.exe
+@IF NOT DEFINED ARE_LOG_STRING SET ARE_LOG_STRING=error_level:WARNING
+
+SET PROFILE_PATH=profile
+SET ARE_OPTIONAL_SERVICES_INI=
+set AUTOSTART_MODEL=
+
+@REM Check commandline parameter
+@IF "%1" == "--webservice" (
+	@    echo "--webservice selected"
+	@    SET AUTOSTART_MODEL=%2
+	@    SET ARE_OPTIONAL_SERVICES_INI="services_websocketdemo.ini"
+) ELSE (
+	@     SET AUTOSTART_MODEL=%1
+)
+
+@echo "Using ARE_LOG_STRING path: %ARE_LOG_STRING%"
+@echo "Using ARE_DEBUG_STRING path: %ARE_DEBUG_STRING%"
+@echo "Using profile path: %PROFILE_PATH%"
+@echo "Using autostart model: %AUTOSTART_MODEL%"
+@echo "Using optional services: %ARE_OPTIONAL_SERVICES_INI%"
+
+@set JAVA_BIN=java
+@IF NOT DEFINED ARE_DEBUG_STRING SET JAVA_BIN=javaw
+
+@if exist java\bin\java.exe (
+	@set JAVA_BIN=java\bin\java.exe
+	@IF NOT DEFINED ARE_DEBUG_STRING SET JAVA_BIN=java\bin\javaw
 )
 
 %JAVA_BIN% -version 2>&1  | jtester.exe
@@ -16,18 +41,43 @@ if exist java\bin\java.exe (
 @ECHO OFF
 IF ERRORLEVEL 1 GOTO QuitError
 
+%JAVA_BIN% -jar VCChecker.jar
+IF ERRORLEVEL 1 GOTO ContARE
+echo NO C++ Redistributable Package found. 
+echo It is highly recommended to download and install the Visual C++ Redistributable Package from Microsoft!
+echo Download link for 32 bit systems: http://www.microsoft.com/en-us/download/details.aspx?id=5555
+echo Several Plugins will not work without the package!
+rem GOTO QuitError
+Pause
+
+:ContARE
+
 IF EXIST asterics.ARE.jar GOTO Continue
 ECHO ARE jar files not found, please unzip ARE.zip or copy jar files into this folder !
 GOTO QuitError
 
 :Continue
-ECHO Starting AsTeRICS Runtime Environment ...
-ECHO error_level:WARNING>.logger
 
-start %JAVA_BIN% -splash:images/asterics_startup.png -Djava.util.logging.config.file=logging.properties -Dosgi.clean=true -Dorg.osgi.framework.bootdelegation=* -Deu.asterics.ARE.startModel=%1 -Deu.asterics.ARE.ServicesFiles="services.ini;services-windows.ini" -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration profile -console
-REM %JAVA_BIN% -Djava.util.logging.config.file=logging.properties -Dorg.osgi.framework.bootdelegation=* -Deu.asterics.ARE.startModel=%1 -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration profile -console
+ECHO Deleting OSGi-Cache
+rd /s/q %PROFILE_PATH%\org.eclipse.osgi
+
+ECHO Starting AsTeRICS Runtime Environment...
+ECHO %ARE_LOG_STRING%>.logger
+REM ECHO error_level:FINE>.logger
+
+set START_CMD=
+set SPLASH_SWITCH=
+@IF NOT DEFINED ARE_DEBUG_STRING set START_CMD=start
+@IF NOT DEFINED ARE_DEBUG_STRING set SPLASH_SWITCH=-splash:images/asterics_startup.png
+
+%START_CMD% %JAVA_BIN% %ARE_DEBUG_STRING% %SPLASH_SWITCH% -Dosgi.clean=true -Dorg.osgi.framework.bootdelegation=* -Dorg.osgi.framework.system.packages.extra=sun.misc -DAnsi=true -Djava.util.logging.config.file=logging.properties -Deu.asterics.ARE.startModel=%AUTOSTART_MODEL% -Deu.asterics.ARE.ServicesFiles="services.ini;services-windows.ini;%ARE_OPTIONAL_SERVICES_INI%" -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration %PROFILE_PATH% -console
+
+set ARE_LOG_STRING= 
+set ARE_DEBUG_STRING=
+
 GOTO Quit
 
 :QuitError
 pause
+
 :Quit
--- bin/ARE/start_debug.bat
+++ bin/ARE/start_debug.bat
@@ -1,70 +1,7 @@
-:Initialization
+set ARE_LOG_STRING=error_level:FINE
+set ARE_DEBUG_STRING="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044"
 
-@REM VERY IMPORTANT: The two lines ensure that the script is executed in the current directy (ARE), also when called
-@REM as administrator
-@setlocal enableextensions
-@cd /d "%~dp0"
+start_new.bat %*
 
-CLS
-
-SET PROFILE_PATH=profile
-SET ARE_OPTIONAL_SERVICES_INI=
-
-@REM Check commandline parameter
-@IF "%1" == "--webservice" (
-	@    echo "--webservice selected"
-	@    SET AUTOSTART_MODEL=%2
-	@    SET ARE_OPTIONAL_SERVICES_INI="services_websocketdemo.ini"
-
-) ELSE (
-	@     SET AUTOSTART_MODEL=%1
-)
-
-@echo "Using profile path: %PROFILE_PATH%"
-@echo "Using autostart model: %AUTOSTART_MODEL%"
-@echo "Using optional services: %ARE_OPTIONAL_SERVICES_INI%"
-
-set JAVA_BIN="java"
-
-if exist java\bin\java.exe (
-	set JAVA_BIN=java\bin\java.exe
-)
-
-%JAVA_BIN% -version 2>&1  | jtester.exe
-
-@ECHO OFF
-IF ERRORLEVEL 1 GOTO QuitError
-
-%JAVA_BIN% -jar VCChecker.jar
-IF ERRORLEVEL 1 GOTO ContARE
-echo NO C++ Redistributable Package found. 
-echo It is highly recommended to download and install the Visual C++ Redistributable Package from Microsoft!
-echo Download link for 32 bit systems: http://www.microsoft.com/en-us/download/details.aspx?id=5555
-echo Several Plugins will not work without the package!
-rem GOTO QuitError
-Pause
-
-:ContARE
-
-IF EXIST asterics.ARE.jar GOTO Continue
-ECHO ARE jar files not found, please unzip ARE.zip or copy jar files into this folder !
-GOTO QuitError
-
-:Continue
-
-ECHO Deleting OSGi-Cache
-rd /s/q %PROFILE_PATH%\org.eclipse.osgi
-
-ECHO Starting AsTeRICS Runtime Environment with Debug output ...
-ECHO error_level:FINE>.logger
-%JAVA_BIN% -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -Dosgi.clean=true -Dorg.osgi.framework.bootdelegation=* -Dorg.osgi.framework.system.packages.extra=sun.misc -DAnsi=true -Djava.util.logging.config.file=logging.properties -Deu.asterics.ARE.startModel=%AUTOSTART_MODEL% -Deu.asterics.ARE.ServicesFiles="services.ini;services-windows.ini;%ARE_OPTIONAL_SERVICES_INI%" -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration %PROFILE_PATH% -console
-
-REM If you want to enable Java Mission Control and Flight Recording flags use the line below
-REM %JAVA_BIN% -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -Dorg.osgi.framework.bootdelegation=* -DAnsi=true -Djava.util.logging.config.file=logging.properties  -jar org.eclipse.osgi_3.6.0.v20100517.jar -configuration %PROFILE_PATH% -console
-
-GOTO Quit
-
-:QuitError
-pause
-
-:Quit
+set ARE_LOG_STRING= 
+set ARE_DEBUG_STRING=
