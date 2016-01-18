e06b21cf0691676731b50af9e033358efc18b3e5
deinhofer <deinhofe@technikum-wien.at>
2016-01-18 09:52:41 +0100

    cleaned up import statements

--- ARE/tools/APE/src/main/java/eu/asterics/ape/main/APE.java
+++ ARE/tools/APE/src/main/java/eu/asterics/ape/main/APE.java
@@ -1,23 +1,20 @@
 package eu.asterics.ape.main;
 
-import java.awt.EventQueue;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map.Entry;
+import java.util.Properties;
 import java.util.logging.Level;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
 import org.xml.sax.SAXException;
-
-import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import com.sun.org.apache.xalan.internal.utils.XMLSecurityPropertyManager.Property;
 
 import eu.asterics.ape.packaging.Packager;
 import eu.asterics.ape.parse.ModelInspector;
