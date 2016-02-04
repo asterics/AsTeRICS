cdd54af26dacfe95d8f9bb6dfda97f2d7f57fd0d
Simon Bates <simon@bitstructures.com>
2016-02-04 10:07:22 -0500

    NexusConnector WebSocket connection

--- ARE/components/processor.nexusconnector/src/main/java/eu/asterics/component/processor/nexusconnector/NexusConnectorInstance.java
+++ ARE/components/processor.nexusconnector/src/main/java/eu/asterics/component/processor/nexusconnector/NexusConnectorInstance.java
@@ -40,6 +40,15 @@ import eu.asterics.mw.model.runtime.impl.DefaultRuntimeInputPort;
 import eu.asterics.mw.model.runtime.impl.DefaultRuntimeEventTriggererPort;
 import eu.asterics.mw.services.AstericsErrorHandling;
 import eu.asterics.mw.services.AREServices;
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
+import com.ning.http.client.ws.WebSocket;
+import com.ning.http.client.ws.WebSocketUpgradeHandler;
+import com.ning.http.client.ws.WebSocketTextListener;
+import com.ning.http.client.ListenableFuture;
+import com.ning.http.client.providers.grizzly.GrizzlyAsyncHttpProvider;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
 
 /**
  * 
@@ -53,57 +62,50 @@ import eu.asterics.mw.services.AREServices;
  */
 public class NexusConnectorInstance extends AbstractRuntimeComponentInstance
 {
-	// Usage of an output port e.g.: opMyOutPort.sendData(ConversionUtils.intToBytes(10)); 
+    // Usage of an output port e.g.: opMyOutPort.sendData(ConversionUtils.intToBytes(10)); 
 
-	// Usage of an event trigger port e.g.: etpMyEtPort.raiseEvent();
+    // Usage of an event trigger port e.g.: etpMyEtPort.raiseEvent();
 
 
-	// declare member variables here
+    // declare member variables here
+    AsyncHttpClientConfig nexusClientConfig = new AsyncHttpClientConfig.Builder().build();
+    AsyncHttpClient nexusClient = new AsyncHttpClient(new GrizzlyAsyncHttpProvider(nexusClientConfig), nexusClientConfig);
+    WebSocket nexusWebSocket;
 
-  
-    
-   /**
-    * The class constructor.
-    */
-    public NexusConnectorInstance()
-    {
+    /**
+     * The class constructor.
+     */
+    public NexusConnectorInstance() {
         // empty constructor
     }
 
-   /**
-    * returns an Input Port.
-    * @param portID   the name of the port
-    * @return         the input port or null if not found
-    */
-    public IRuntimeInputPort getInputPort(String portID)
-    {
-		if ("myInPort".equalsIgnoreCase(portID))
-		{
-			return ipMyInPort;
-		}
-
-		return null;
-	}
+    /**
+     * returns an Input Port.
+     * @param portID   the name of the port
+     * @return         the input port or null if not found
+     */
+    public IRuntimeInputPort getInputPort(String portID) {
+        if ("myInPort".equalsIgnoreCase(portID)) {
+            return ipMyInPort;
+        }
+        return null;
+    }
 
     /**
      * returns an Output Port.
      * @param portID   the name of the port
      * @return         the output port or null if not found
      */
-    public IRuntimeOutputPort getOutputPort(String portID)
-	{
-
-		return null;
-	}
+    public IRuntimeOutputPort getOutputPort(String portID) {
+        return null;
+    }
 
     /**
      * returns an Event Listener Port.
      * @param eventPortID   the name of the port
      * @return         the EventListener port or null if not found
      */
-    public IRuntimeEventListenerPort getEventListenerPort(String eventPortID)
-    {
-
+    public IRuntimeEventListenerPort getEventListenerPort(String eventPortID) {
         return null;
     }
 
@@ -112,20 +114,16 @@ public class NexusConnectorInstance extends AbstractRuntimeComponentInstance
      * @param eventPortID   the name of the port
      * @return         the EventTriggerer port or null if not found
      */
-    public IRuntimeEventTriggererPort getEventTriggererPort(String eventPortID)
-    {
-
+    public IRuntimeEventTriggererPort getEventTriggererPort(String eventPortID) {
         return null;
     }
-		
+
     /**
      * returns the value of the given property.
      * @param propertyName   the name of the property
      * @return               the property value or null if not found
      */
-    public Object getRuntimePropertyValue(String propertyName)
-    {
-
+    public Object getRuntimePropertyValue(String propertyName) {
         return null;
     }
 
@@ -134,68 +132,93 @@ public class NexusConnectorInstance extends AbstractRuntimeComponentInstance
      * @param propertyName   the name of the property
      * @param newValue       the desired property value or null if not found
      */
-    public Object setRuntimePropertyValue(String propertyName, Object newValue)
-    {
-
+    public Object setRuntimePropertyValue(String propertyName, Object newValue) {
         return null;
     }
 
-     /**
-      * Input Ports for receiving values.
-      */
-	private final IRuntimeInputPort ipMyInPort  = new DefaultRuntimeInputPort()
-	{
-		public void receiveData(byte[] data)
-		{
-				 // insert data reception handling here, e.g.: 
-				 // myVar = ConversionUtils.doubleFromBytes(data); 
-				 // myVar = ConversionUtils.stringFromBytes(data); 
-				 // myVar = ConversionUtils.intFromBytes(data); 
-		}
-	};
+    /**
+     * Input Ports for receiving values.
+     */
+    private final IRuntimeInputPort ipMyInPort = new DefaultRuntimeInputPort() {
+        public void receiveData(byte[] data) {
+            // insert data reception handling here, e.g.:
+            // myVar = ConversionUtils.doubleFromBytes(data);
+            // myVar = ConversionUtils.stringFromBytes(data);
+            // myVar = ConversionUtils.intFromBytes(data);
+            double val = ConversionUtils.doubleFromBytes(data);
+            // System.out.println("NexusConnector IN = " + val);
+            nexusWebSocket.sendMessage("{ \"path\": \"\", \"value\": " + val + " }");
+        }
+    };
 
 
-     /**
-      * Event Listerner Ports.
-      */
+    /**
+     * Event Listerner Ports.
+     */
 
-	
 
-     /**
-      * called when model is started.
-      */
-      @Override
-      public void start()
-      {
+    /**
+     * called when model is started.
+     */
+    @Override
+    public void start() {
+        super.start();
+        System.out.println("NexusConnector START");
 
-          super.start();
-      }
+        ListenableFuture<WebSocket> f = nexusClient.prepareGet("ws://localhost:9081/bindModel/example1/a")
+            .execute(new WebSocketUpgradeHandler.Builder().addWebSocketListener(new WebSocketTextListener() {
+                    @Override
+                    public void onMessage(String message) {
+                    }
 
-     /**
-      * called when model is paused.
-      */
-      @Override
-      public void pause()
-      {
-          super.pause();
-      }
+                    @Override
+                    public void onOpen(WebSocket websocket) {
+                    }
 
-     /**
-      * called when model is resumed.
-      */
-      @Override
-      public void resume()
-      {
-          super.resume();
-      }
+                    @Override
+                    public void onClose(WebSocket websocket) {
+                    }
 
-     /**
-      * called when model is stopped.
-      */
-      @Override
-      public void stop()
-      {
+                    @Override
+                    public void onError(Throwable t) {
+                    }
+            }).build());
 
-          super.stop();
-      }
+        try {
+            nexusWebSocket = f.get();
+        } catch (CancellationException e) {
+            // TODO: Proper Exception handling
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            // TODO: Proper Exception handling
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            // TODO: Do something with the Exception
+        }
+    }
+
+    /**
+     * called when model is paused.
+     */
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    /**
+     * called when model is resumed.
+     */
+    @Override
+    public void resume() {
+        super.resume();
+    }
+
+    /**
+     * called when model is stopped.
+     */
+    @Override
+    public void stop() {
+        super.stop();
+        System.out.println("NexusConnector STOP");
+    }
 }
