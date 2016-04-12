f760f72f09c7dc650a1d702d3bad0f04f9a7e068
Simon Bates <simon@bitstructures.com>
2016-04-12 17:12:33 -0400

    GPII-1543: Add a string output parameter to NexusConnector

--- ARE/components/processor.nexusconnector/src/main/java/eu/asterics/component/processor/nexusconnector/NexusConnectorInstance.java
+++ ARE/components/processor.nexusconnector/src/main/java/eu/asterics/component/processor/nexusconnector/NexusConnectorInstance.java
@@ -1,29 +1,8 @@
-
-
 /*
- *    AsTeRICS - Assistive Technology Rapid Integration and Construction Set
- * 
- * 
- *        d8888      88888888888       8888888b.  8888888 .d8888b.   .d8888b. 
- *       d88888          888           888   Y88b   888  d88P  Y88b d88P  Y88b
- *      d88P888          888           888    888   888  888    888 Y88b.     
- *     d88P 888 .d8888b  888   .d88b.  888   d88P   888  888         "Y888b.  
- *    d88P  888 88K      888  d8P  Y8b 8888888P"    888  888            "Y88b.
- *   d88P   888 "Y8888b. 888  88888888 888 T88b     888  888    888       "888
- *  d8888888888      X88 888  Y8b.     888  T88b    888  Y88b  d88P Y88b  d88P
- * d88P     888  88888P' 888   "Y8888  888   T88b 8888888 "Y8888P"   "Y8888P" 
- *
- *
- *                    homepage: http://www.asterics.org 
- *
- *       This project has been partly funded by the European Commission, 
- *                      Grant Agreement Number 247730
- *  
- *  
- *    License: GPL v3.0 (GNU General Public License Version 3.0)
- *                 http://www.gnu.org/licenses/gpl.html
- * 
- */
+Copyright 2016 OCAD University
+
+Licensed under the New BSD license.
+*/
 
 package eu.asterics.component.processor.nexusconnector;
 
@@ -49,6 +28,8 @@ import com.ning.http.client.ListenableFuture;
 import com.ning.http.client.providers.grizzly.GrizzlyAsyncHttpProvider;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonValue;
 
 /**
  * 
@@ -103,6 +84,9 @@ public class NexusConnectorInstance extends AbstractRuntimeComponentInstance
      * @return         the output port or null if not found
      */
     public IRuntimeOutputPort getOutputPort(String portID) {
+        if ("outStrOne".equalsIgnoreCase(portID)) {
+            return opStrOne;
+        }
         return null;
     }
 
@@ -143,35 +127,41 @@ public class NexusConnectorInstance extends AbstractRuntimeComponentInstance
     }
 
     /**
-     * Input Ports for receiving values.
+     * Input Ports
      */
     private final IRuntimeInputPort ipA = new DefaultRuntimeInputPort() {
         public void receiveData(byte[] data) {
             double val = ConversionUtils.doubleFromBytes(data);
-            nexusWebSocket.sendMessage("{ \"path\": \"a\", \"value\": " + val + " }");
+            nexusWebSocket.sendMessage("{ \"path\": \"inputs.a\", \"value\": " + val + " }");
         }
     };
 
     private final IRuntimeInputPort ipB = new DefaultRuntimeInputPort() {
         public void receiveData(byte[] data) {
             double val = ConversionUtils.doubleFromBytes(data);
-            nexusWebSocket.sendMessage("{ \"path\": \"b\", \"value\": " + val + " }");
+            nexusWebSocket.sendMessage("{ \"path\": \"inputs.b\", \"value\": " + val + " }");
         }
     };
 
     private final IRuntimeInputPort ipC = new DefaultRuntimeInputPort() {
         public void receiveData(byte[] data) {
             double val = ConversionUtils.doubleFromBytes(data);
-            nexusWebSocket.sendMessage("{ \"path\": \"c\", \"value\": " + val + " }");
+            nexusWebSocket.sendMessage("{ \"path\": \"inputs.c\", \"value\": " + val + " }");
         }
     };
 
     private final IRuntimeInputPort ipD = new DefaultRuntimeInputPort() {
         public void receiveData(byte[] data) {
             double val = ConversionUtils.doubleFromBytes(data);
-            nexusWebSocket.sendMessage("{ \"path\": \"d\", \"value\": " + val + " }");
+            nexusWebSocket.sendMessage("{ \"path\": \"inputs.d\", \"value\": " + val + " }");
         }
     };
+
+    /**
+     * Output Ports
+     */
+    private String opStrOneValue = null;
+    private final IRuntimeOutputPort opStrOne = new DefaultRuntimeOutputPort();
 
 
     /**
@@ -187,10 +177,25 @@ public class NexusConnectorInstance extends AbstractRuntimeComponentInstance
         super.start();
         System.out.println("NexusConnector START");
 
-        ListenableFuture<WebSocket> f = nexusClient.prepareGet("ws://localhost:9081/bindModel/nexus.asterics/inputs")
+        ListenableFuture<WebSocket> f = nexusClient.prepareGet("ws://localhost:9081/bindModel/nexus.asterics/connector")
             .execute(new WebSocketUpgradeHandler.Builder().addWebSocketListener(new WebSocketTextListener() {
                     @Override
                     public void onMessage(String message) {
+                        JsonValue changeMessage = Json.parse(message);
+                        if (changeMessage.isObject()) {
+                            JsonValue outputs = changeMessage.asObject().get("outputs");
+                            if (outputs.isObject()) {
+                                JsonValue outStrOneChangeValue = outputs.asObject().get("outStrOne");
+                                if (outStrOneChangeValue.isString()) {
+                                    String outStrOneChangeValueString  = outStrOneChangeValue.asString();
+                                    if (opStrOneValue == null || !opStrOneValue.equals(outStrOneChangeValueString)) {
+                                        System.out.println("NexusConnector OUT STR ONE VALUE CHANGE");
+                                        opStrOneValue = outStrOneChangeValueString;
+                                        opStrOne.sendData(ConversionUtils.stringToBytes(opStrOneValue));
+                                    }
+                                }
+                            }
+                        }
                     }
 
                     @Override
