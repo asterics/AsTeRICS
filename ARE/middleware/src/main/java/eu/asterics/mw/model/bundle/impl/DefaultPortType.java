000b861991a5eef2014b2af6a872a8a3600218f8
Martin Deinhofer <martin.deinhofer@technikum-wien.at>
2016-05-04 15:51:45 +0200

    Changed file headers from
    GPLv3
    to
    MIT or GPLv3 with Exception

--- ARE/middleware/src/main/java/eu/asterics/mw/model/bundle/impl/DefaultPortType.java
+++ ARE/middleware/src/main/java/eu/asterics/mw/model/bundle/impl/DefaultPortType.java
@@ -24,10 +24,10 @@ import java.util.Map;
  *
  *         This project has been funded by the European Commission,
  *                      Grant Agreement Number 247730
- * 
- * 
- *    License: GPL v3.0 (GNU General Public License Version 3.0)
- *                 http://www.gnu.org/licenses/gpl.html
+ *  
+ *  
+ *         Dual License: MIT or GPL v3.0 with "CLASSPATH" exception
+ *         (please refer to the folder LICENSE)
  *
  */
 
--- ARE/middleware/src/main/java/eu/asterics/mw/model/deployment/impl/ModelDescription.java
+++ ARE/middleware/src/main/java/eu/asterics/mw/model/deployment/impl/ModelDescription.java
@@ -17,10 +17,10 @@ package eu.asterics.mw.model.deployment.impl;
  *
  *         This project has been funded by the European Commission,
  *                      Grant Agreement Number 247730
- * 
- * 
- *    License: GPL v3.0 (GNU General Public License Version 3.0)
- *                 http://www.gnu.org/licenses/gpl.html
+ *  
+ *  
+ *         Dual License: MIT or GPL v3.0 with "CLASSPATH" exception
+ *         (please refer to the folder LICENSE)
  *
  */
 public class ModelDescription {
