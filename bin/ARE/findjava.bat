a753e9ed2da2a7e52797df9d609b6f0b43ce2d7d
Martin Deinhofer <martin.deinhofer@technikum-wien.at>
2016-05-23 16:39:42 +0200

    Improved #100: In some cases the JRE is not registered in the registry, so added additional search strategies. In total there are 7 now:
    1) in case this is an installed AsTeRICS with an embedded java in the java subfolder
    2) otherwise, first search for the current 32bit jre installed in the registry
    3) otherwise, search for the current 32bit jdk having an embedded jre installed in the registry
    4) next, try the programdata symbolic link
    5) next, search for an entry in the %ProgramFiles(x86)% subfolder, because this is the normal location for java 32-bit installations
    6) if we could not find anything else, search in the 64-bit  standard installation folder
    7) Try if %JAVA_EXE% is in path, because this could be any other than the standard paths in theory.
    
    The strategies are tried in the given order. As soon as one is successful the java.exe is checked for the correct version (>=1.7) and the full path is returned

--- bin/ARE/findjava.bat
+++ bin/ARE/findjava.bat
@@ -7,16 +7,18 @@ REM author: Martin Deinhofer, mailtto:martin.deinhofer@technikum-wien.at
 
 
 setlocal
+REM echo "params: %1, %2"
+set JAVA_EXE=%2
 
 REM uncomment for testing the single strategies
-REM goto :one
+REM goto :seven
 
 :one
 REM 1) in case this is an installed AsTeRICS with an embedded java in the java subfolder
-if exist "java\bin\java.exe" (
-	set "java_bin=java\bin"
-	call :foundjava "Step One"
-	goto:eof
+if exist "java\bin\%JAVA_EXE%" (
+	set java_bin="java\bin\%JAVA_EXE%"
+	echo Step One, found java path:
+	goto foundjava
 )
 
 :two
@@ -24,57 +26,73 @@ REM 2) otherwise, first search for the current 32bit jre installed
 set "branch=HKLM\Software\JavaSoft\Java Runtime Environment"
 call :findjava32 jre
 
-if exist "%jre%\bin\java.exe" (
-	set "java_bin=%jre%\bin"
-	call :foundjava "Step Two"
-	goto:eof
+if exist "%jre%\bin\%JAVA_EXE%" (
+	set java_bin="%jre%\bin\%JAVA_EXE%"
+	echo Step Two, found java path:
+	goto foundjava	
 )
 
 :three
 REM 3) otherwise, search for the current 32bit jdk having an embedded jre installed
 set "branch=HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\JavaSoft\Java Development Kit"
 call :findjava32 jre
-if exist "%jre%\jre\bin\java.exe" (
-	set "java_bin=%jre%\jre\bin"
-	call :foundjava "Step Three"
-	goto:eof
+if exist "%jre%\jre\bin\%JAVA_EXE%" (
+	set java_bin="%jre%\jre\bin\%JAVA_EXE%"
+	echo Step Three, found java path:
+	goto foundjava
 )
 
 :four
 REM 4) next, try the programdata symbolic link
-if exist "%ProgramData%\Oracle\Java\javapath32\java.exe" (
-	set "java_bin=%ProgramData%\Oracle\Java\javapath32"
-	call :foundjava "Step Four"
-	goto:eof
+if exist "%ProgramData%\Oracle\Java\javapath32\%JAVA_EXE%" (
+	set java_bin="%ProgramData%\Oracle\Java\javapath32\%JAVA_EXE%"
+	echo Step Four, found java path:
+	goto foundjava
 )
 
 :five
 REM 5) next, search for an entry in the %ProgramFiles(x86)% subfolder, because this is the normal location for java 32-bit installations
 FOR /D %%d IN ("%ProgramFiles(x86)%\Java\*") DO (
-	if exist "%%d\bin\java.exe" (
-		set "java_bin=%%d\bin"
-		call :foundjava "Step Five"
-		goto:eof
+	if exist "%%d\bin\%JAVA_EXE%" (
+		set java_bin="%%d\bin\%JAVA_EXE%"
+		echo Step Five, found java path:
+		goto foundjava
 	)
 )
 
 :six
 REM 6) Finally, if we could not find anything else, search in the 64-bit  standard installation folder
 FOR /D %%d IN ("%ProgramFiles%\Java\*") DO (
-	if exist "%%d\bin\java.exe" (
-		set "java_bin=%%d\bin"
-		call :foundjava "Step Six"
-		goto:eof
+	if exist "%%d\bin\%JAVA_EXE%" (
+		set java_bin="%%d\bin\%JAVA_EXE%"
+		echo Step Six, found java path:
+		goto foundjava
 	)
 )
 
+:seven
+REM 7) Try if %JAVA_EXE% is in path, because this could be any other than the standard paths in theory.
+set java_bin="%JAVA_EXE%"
+echo Step Seven, found java path:
+goto foundjava
+
+
 :nojava
 REM As I don't know of a possibility to popup a message, use "start" to popup a new batch command window. This is a trick to force the user recognizing the message.
-start echo "No Java found, please install a 32-bit Java Runtime Environment: http://www.java.com/de/download/manual.jsp"
+start echo "No Java (>= 1.7, 32-bit) found, please install a correct Java Runtime Environment: http://www.java.com/de/download/manual.jsp"
 exit /b 1
 
 :foundjava
-echo Found java path through %1: "%java_bin%"
+echo %java_bin%
+
+REM test, if java is ok
+REM ERRORLEVEL: 9009: command not found --> path, program not existent or executable
+REM ERRORLEVEL: 1: jre version < 1.7
+REM ERRORLEVEL: 0: JRE OK
+%java_bin% -version 2>&1 | jtester.exe
+REM echo java version "1.6.0_12" |jtester.exe
+IF NOT ERRORLEVEL 0 goto :nojava
+
 endlocal&set "%~1=%java_bin%"
 exit /b 0
 
