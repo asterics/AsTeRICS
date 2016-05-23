5ce82d5b3e94e504d3650bf6492670af0aa5be26
Martin Deinhofer <martin.deinhofer@technikum-wien.at>
2016-05-23 16:42:22 +0200

    added @setlocal enableextensions @cd /d "%~dp0"
    at the beginnging of the file to make used environment variables with local scope only and change to directory of the script
    Before as soon as somebody started start_debug.bat using start.bat in a subsequent call also started with debug options

--- bin/ARE/start_debug.bat
+++ bin/ARE/start_debug.bat
@@ -1,3 +1,8 @@
+@REM VERY IMPORTANT: The two lines ensure that the script is executed in the current directy (ARE), also when called
+@REM as administrator
+@setlocal enableextensions
+@cd /d "%~dp0"
+
 set ARE_LOG_STRING=error_level:FINE
 set ARE_DEBUG_STRING="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044"
 
