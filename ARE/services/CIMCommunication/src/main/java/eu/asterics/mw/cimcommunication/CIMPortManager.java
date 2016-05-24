ff7b7da5794b358c6b9f3d5c1a0f08907a1a3e8c
Martin Deinhofer <martin.deinhofer@technikum-wien.at>
2016-05-24 22:27:17 +0200

    fixed model start error that was caused by a CimPortManager which failed loading the RXTX lib. --> Only register for ARE events and systemstate changes if loading succeeds

--- ARE/services/CIMCommunication/src/main/java/eu/asterics/mw/cimcommunication/CIMPortManager.java
+++ ARE/services/CIMCommunication/src/main/java/eu/asterics/mw/cimcommunication/CIMPortManager.java
@@ -87,8 +87,6 @@ public class CIMPortManager implements IAREEventListener, SystemChangeListener
 		logger = AstericsErrorHandling.instance.getLogger();
 		// logger.fine("Constructing CIM Port Manager");
 		
-		AREServices.instance.registerAREEventListener(this);
-		SystemChangeNotifier.instance.addListener(this);
 	
 		comPorts = new Hashtable<CIMUniqueIdentifier, CIMPortController>();
 		comRawPorts = new HashMap<String, CIMPortController>();
@@ -96,6 +94,11 @@ public class CIMPortManager implements IAREEventListener, SystemChangeListener
 		generateCIMDescriptionMap();
 		
 		rescan(AUTODETECT_WAIT_TIME);
+
+		logger.fine("Scanning of ports successful, so register for ARE events, and systemstate changes");
+		AREServices.instance.registerAREEventListener(this);
+		SystemChangeNotifier.instance.addListener(this);
+
     }
 
 	/**
