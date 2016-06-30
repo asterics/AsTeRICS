6a1df7af67d60cf013724dbe419ad3f845822ba4
Martin Deinhofer <martin.deinhofer@technikum-wien.at>
2016-06-30 17:21:55 +0200

    added intel real sense plugin

---
+++ ARE/components/sensor.intelrealsense/src/main/java/eu/asterics/component/sensor/intelrealsense/BridgeIntelRealSense.java
@@ -0,0 +1,156 @@
+/*
+ *    AsTeRICS - Assistive Technology Rapid Integration and Construction Set
+ * 
+ * 
+ *        d8888      88888888888       8888888b.  8888888 .d8888b.   .d8888b. 
+ *       d88888          888           888   Y88b   888  d88P  Y88b d88P  Y88b
+ *      d88P888          888           888    888   888  888    888 Y88b.     
+ *     d88P 888 .d8888b  888   .d88b.  888   d88P   888  888         "Y888b.  
+ *    d88P  888 88K      888  d8P  Y8b 8888888P"    888  888            "Y88b.
+ *   d88P   888 "Y8888b. 888  88888888 888 T88b     888  888    888       "888
+ *  d8888888888      X88 888  Y8b.     888  T88b    888  Y88b  d88P Y88b  d88P
+ * d88P     888  88888P' 888   "Y8888  888   T88b 8888888 "Y8888P"   "Y8888P" 
+ *
+ *
+ *                    homepage: http://www.asterics.org 
+ *
+ *       This project has been partly funded by the European Commission, 
+ *                      Grant Agreement Number 247730
+ *  
+ *  
+ *         Dual License: MIT or GPL v3.0 with "CLASSPATH" exception
+ *         (please refer to the folder LICENSE)
+ * 
+ */
+
+package eu.asterics.component.sensor.intelrealsense;
+
+import eu.asterics.mw.data.ConversionUtils;
+
+public class BridgeIntelRealSense {
+	
+	private IntelRealSenseInstance owner;
+	
+	final int BROW_RAISER_LEFT	= 1;
+	final int BROW_RAISER_RIGHT	= 0;
+	final int BROW_LOWERER_LEFT	= 3;
+	final int BROW_LOWERER_RIGHT= 2;
+	final int SMILE 			= 4;
+	final int KISS 				= 5;
+	final int MOUTH_OPEN 		= 6;
+	final int TONGUE_OUT 		= 7;
+	final int EYES_CLOSED_LEFT 	= 9;
+	final int EYES_CLOSED_RIGHT	= 8;
+	final int EYES_TURND_LEFT	= 11;
+	final int EYES_TURND_RIGHT	= 10;
+	final int EYES_UP			= 12;
+	final int EYES_DOWN			= 13;
+	final int PUFF_LEFT			= 15;
+	final int PUFF_RIGHT		= 14;
+	
+	static {
+		System.out.println("Trying to load native library...");
+		System.loadLibrary("IntelRealSense");
+		//System.load("C:/AsTeRICS_PREP/ARE/components/sensor.realsense/src/main/resources/lib/native/IntelF200.dll");
+		System.out.println("Native lib loaded successfully");
+	}
+	//interface definition for f200 communication
+	native public int init(int devNr, int enableExpressions, int displayGUI);
+	native public void deactivate();
+	native public void startTracking();
+	native public void pause();
+	native public void resume();
+	
+	// The class constructor.
+	public BridgeIntelRealSense(final IntelRealSenseInstance owner)
+    {
+		this.owner = owner;
+    }
+	/*
+	public void start(int devNr,int enableExpressions,int displayGUI) {
+
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				for(int i=0;i<1;i++){
+					if(BridgeF200.this.init(devNr,enableExpressions,displayGUI)==-1){
+						System.out.println("Init failed");break;
+					}
+					System.out.println("Init passed");
+					BridgeF200.this.startTracking();
+				}
+			}
+		}).start();
+
+	}*/
+	
+
+	//Callback from cpp -> java
+	private void poseDataCallback(int h, int w, int x, int y, int roll, int yaw, int pitch) {
+		// h ... The rectangle height in pixels.
+		// w ... The rectangle width in pixels.
+		// x ... The horizontal coordinate of the top left pixel of the rectangle.
+		// y ... The vertical coordinate of the top left pixel of the rectangle.
+		owner.opH.sendData(ConversionUtils.intToBytes(h));
+		owner.opW.sendData(ConversionUtils.intToBytes(w));
+		owner.opX.sendData(ConversionUtils.intToBytes(x));
+		owner.opY.sendData(ConversionUtils.intToBytes(y));
+		owner.opRoll.sendData(ConversionUtils.intToBytes(roll));
+		owner.opYaw.sendData(ConversionUtils.intToBytes(yaw));
+		owner.opPitch.sendData(ConversionUtils.intToBytes(pitch));
+	}
+	//Callback expressions
+	private void expressionCallback(int[] expressionScores) {
+		if(expressionScores[BROW_RAISER_LEFT]>owner.propScoreBrowRaiserLeft){
+			owner.etpBrowRaiserLeft.raiseEvent();
+		};
+		if(expressionScores[BROW_RAISER_RIGHT]>owner.propScoreBrowRaiserRight){
+			owner.etpBrowRaiserRight.raiseEvent();
+		};
+		if(expressionScores[BROW_LOWERER_LEFT]>owner.propScoreBrowLowererLeft){
+			owner.etpBrowLowererLeft.raiseEvent();
+		};
+		if(expressionScores[BROW_LOWERER_RIGHT]>owner.propScoreBrowLowererRight){
+			owner.etpBrowLowererRight.raiseEvent();
+		};
+		if(expressionScores[SMILE]>owner.propScoreSmile){
+			owner.etpSmile.raiseEvent();
+		};
+		if(expressionScores[KISS]>owner.propScoreKiss){
+			owner.etpKiss.raiseEvent();
+		};
+		if(expressionScores[MOUTH_OPEN]>owner.propScoreMouthOpen){
+			owner.etpMouthOpen.raiseEvent();
+		};
+		if(expressionScores[TONGUE_OUT]>owner.propScoreThongueOut){
+			owner.etpThongueOut.raiseEvent();
+		};
+		if(expressionScores[EYES_CLOSED_LEFT]>owner.propScoreEyesClosedLeft){
+			owner.etpEyesClosedLeft.raiseEvent();
+		}; 
+		if(expressionScores[EYES_CLOSED_RIGHT]>owner.propScoreEyesClosedRight){
+			owner.etpEyesClosedRight.raiseEvent();
+		};
+		if(expressionScores[EYES_TURND_LEFT]>owner.propScoreEyesTurnLeft){
+			owner.etpEyesTurnLeft.raiseEvent();
+		};
+		if(expressionScores[EYES_TURND_RIGHT]>owner.propScoreEyesTurnRight){
+			owner.etpEyesTurnRight.raiseEvent();
+		};
+		if(expressionScores[EYES_UP]>owner.propScoreEyesUp){
+			owner.etpEyesUp.raiseEvent();
+		};
+		if(expressionScores[EYES_DOWN]>owner.propScoreEyesDown){
+			owner.etpEyesDown.raiseEvent();
+		};
+		if(expressionScores[PUFF_LEFT]>owner.propScorePuffLeft){
+			owner.etpPuffLeft.raiseEvent();
+		};
+		if(expressionScores[PUFF_RIGHT]>owner.propScorePuffRight){
+			owner.etpPuffRight.raiseEvent();
+		};
+	}
+
+
+}
---
+++ ARE/components/sensor.intelrealsense/src/main/java/eu/asterics/component/sensor/intelrealsense/IntelRealSenseInstance.java
@@ -0,0 +1,563 @@
+
+
+/*
+ *    AsTeRICS - Assistive Technology Rapid Integration and Construction Set
+ * 
+ * 
+ *        d8888      88888888888       8888888b.  8888888 .d8888b.   .d8888b. 
+ *       d88888          888           888   Y88b   888  d88P  Y88b d88P  Y88b
+ *      d88P888          888           888    888   888  888    888 Y88b.     
+ *     d88P 888 .d8888b  888   .d88b.  888   d88P   888  888         "Y888b.  
+ *    d88P  888 88K      888  d8P  Y8b 8888888P"    888  888            "Y88b.
+ *   d88P   888 "Y8888b. 888  88888888 888 T88b     888  888    888       "888
+ *  d8888888888      X88 888  Y8b.     888  T88b    888  Y88b  d88P Y88b  d88P
+ * d88P     888  88888P' 888   "Y8888  888   T88b 8888888 "Y8888P"   "Y8888P" 
+ *
+ *
+ *                    homepage: http://www.asterics.org 
+ *
+ *       This project has been partly funded by the European Commission, 
+ *                      Grant Agreement Number 247730
+ *  
+ *  
+ *         Dual License: MIT or GPL v3.0 with "CLASSPATH" exception
+ *         (please refer to the folder LICENSE)
+ * 
+ */
+
+package eu.asterics.component.sensor.intelrealsense;
+
+
+import java.util.concurrent.Callable;
+import java.util.logging.Logger;
+import eu.asterics.mw.data.ConversionUtils;
+import eu.asterics.mw.model.runtime.AbstractRuntimeComponentInstance;
+import eu.asterics.mw.model.runtime.IRuntimeInputPort;
+import eu.asterics.mw.model.runtime.IRuntimeOutputPort;
+import eu.asterics.mw.model.runtime.IRuntimeEventListenerPort;
+import eu.asterics.mw.model.runtime.IRuntimeEventTriggererPort;
+import eu.asterics.mw.model.runtime.impl.DefaultRuntimeOutputPort;
+import eu.asterics.mw.model.runtime.impl.DefaultRuntimeInputPort;
+import eu.asterics.mw.model.runtime.impl.DefaultRuntimeEventTriggererPort;
+import eu.asterics.mw.services.AstericsErrorHandling;
+import eu.asterics.mw.services.AstericsThreadPool;
+import eu.asterics.mw.services.AREServices;
+
+/**
+ * 
+ * This plugin interfaces the Intel Real Sense 3D cameras and provide head tracking data and triggers events in case of
+ * facial expressions.
+ * 
+ * 
+ *  
+ * @author Christoph Acker [acker_christoph@yahoo.de]
+ *         Date: 30/06/2016 
+ */
+public class IntelRealSenseInstance extends AbstractRuntimeComponentInstance
+{
+	final IRuntimeOutputPort opH = new DefaultRuntimeOutputPort();
+	final IRuntimeOutputPort opW = new DefaultRuntimeOutputPort();
+	final IRuntimeOutputPort opX = new DefaultRuntimeOutputPort();
+	final IRuntimeOutputPort opY = new DefaultRuntimeOutputPort();
+	final IRuntimeOutputPort opRoll = new DefaultRuntimeOutputPort();
+	final IRuntimeOutputPort opYaw = new DefaultRuntimeOutputPort();
+	final IRuntimeOutputPort opPitch = new DefaultRuntimeOutputPort();
+	// Usage of an output port e.g.: opMyOutPort.sendData(ConversionUtils.intToBytes(10)); 
+
+	final IRuntimeEventTriggererPort etpBrowRaiserLeft = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpBrowRaiserRight = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpBrowLowererLeft = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpBrowLowererRight = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpSmile = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpKiss = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpMouthOpen = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpThongueOut = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpEyesClosedLeft = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpEyesClosedRight = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpEyesTurnLeft = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpEyesTurnRight = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpEyesUp = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpEyesDown = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpPuffLeft = new DefaultRuntimeEventTriggererPort();
+	final IRuntimeEventTriggererPort etpPuffRight = new DefaultRuntimeEventTriggererPort();
+	// Usage of an event trigger port e.g.: etpMyEtPort.raiseEvent();
+
+	int propDeviceModel = 0;
+	boolean propEnableExpressions = true;
+	boolean propdisplayGUI = true;
+	int propScoreBrowRaiserLeft = 50;
+	int propScoreBrowRaiserRight = 50;
+	int propScoreBrowLowererLeft = 50;
+	int propScoreBrowLowererRight = 50;
+	int propScoreSmile = 50;
+	int propScoreKiss = 50;
+	int propScoreMouthOpen = 50;
+	int propScoreThongueOut = 50;
+	int propScoreEyesClosedLeft = 50;
+	int propScoreEyesClosedRight = 50;
+	int propScoreEyesTurnLeft = 50;
+	int propScoreEyesTurnRight = 50;
+	int propScoreEyesUp = 50;
+	int propScoreEyesDown = 50;
+	int propScorePuffLeft = 50;
+	int propScorePuffRight = 50;
+
+	// declare member variables here
+
+	int deviceModels[]=new int[]{2097166,2097167,2097183,2097168};
+	int isExpressionON=0;
+	int showDisplay=0;
+	private final BridgeIntelRealSense bridge = new BridgeIntelRealSense(this);
+    
+   /**
+    * The class constructor.
+    */
+    public IntelRealSenseInstance()
+    {
+        // empty constructor
+    }
+
+   /**
+    * returns an Input Port.
+    * @param portID   the name of the port
+    * @return         the input port or null if not found
+    */
+    public IRuntimeInputPort getInputPort(String portID)
+    {
+
+		return null;
+	}
+
+    /**
+     * returns an Output Port.
+     * @param portID   the name of the port
+     * @return         the output port or null if not found
+     */
+    public IRuntimeOutputPort getOutputPort(String portID)
+	{
+		if ("h".equalsIgnoreCase(portID))
+		{
+			return opH;
+		}
+		if ("w".equalsIgnoreCase(portID))
+		{
+			return opW;
+		}
+		if ("x".equalsIgnoreCase(portID))
+		{
+			return opX;
+		}
+		if ("y".equalsIgnoreCase(portID))
+		{
+			return opY;
+		}
+		if ("roll".equalsIgnoreCase(portID))
+		{
+			return opRoll;
+		}
+		if ("yaw".equalsIgnoreCase(portID))
+		{
+			return opYaw;
+		}
+		if ("pitch".equalsIgnoreCase(portID))
+		{
+			return opPitch;
+		}
+
+		return null;
+	}
+
+    /**
+     * returns an Event Listener Port.
+     * @param eventPortID   the name of the port
+     * @return         the EventListener port or null if not found
+     */
+    public IRuntimeEventListenerPort getEventListenerPort(String eventPortID)
+    {
+
+        return null;
+    }
+
+    /**
+     * returns an Event Triggerer Port.
+     * @param eventPortID   the name of the port
+     * @return         the EventTriggerer port or null if not found
+     */
+    public IRuntimeEventTriggererPort getEventTriggererPort(String eventPortID)
+    {
+		if ("browRaiserLeft".equalsIgnoreCase(eventPortID))
+		{
+			return etpBrowRaiserLeft;
+		}
+		if ("browRaiserRight".equalsIgnoreCase(eventPortID))
+		{
+			return etpBrowRaiserRight;
+		}
+		if ("browLowererLeft".equalsIgnoreCase(eventPortID))
+		{
+			return etpBrowLowererLeft;
+		}
+		if ("browLowererRight".equalsIgnoreCase(eventPortID))
+		{
+			return etpBrowLowererRight;
+		}
+		if ("smile".equalsIgnoreCase(eventPortID))
+		{
+			return etpSmile;
+		}
+		if ("kiss".equalsIgnoreCase(eventPortID))
+		{
+			return etpKiss;
+		}
+		if ("mouthOpen".equalsIgnoreCase(eventPortID))
+		{
+			return etpMouthOpen;
+		}
+		if ("thongueOut".equalsIgnoreCase(eventPortID))
+		{
+			return etpThongueOut;
+		}
+		if ("eyesClosedLeft".equalsIgnoreCase(eventPortID))
+		{
+			return etpEyesClosedLeft;
+		}
+		if ("eyesClosedRight".equalsIgnoreCase(eventPortID))
+		{
+			return etpEyesClosedRight;
+		}
+		if ("eyesTurnRight".equalsIgnoreCase(eventPortID))
+		{
+			return etpEyesTurnRight;
+		}
+		if ("eyesTurnLeft".equalsIgnoreCase(eventPortID))
+		{
+			return etpEyesTurnLeft;
+		}
+		if ("eyesUp".equalsIgnoreCase(eventPortID))
+		{
+			return etpEyesUp;
+		}
+		if ("eyesDown".equalsIgnoreCase(eventPortID))
+		{
+			return etpEyesDown;
+		}
+		if ("puffLeft".equalsIgnoreCase(eventPortID))
+		{
+			return etpPuffLeft;
+		}
+		if ("puffRight".equalsIgnoreCase(eventPortID))
+		{
+			return etpPuffRight;
+		}
+
+        return null;
+    }
+		
+    /**
+     * returns the value of the given property.
+     * @param propertyName   the name of the property
+     * @return               the property value or null if not found
+     */
+    public Object getRuntimePropertyValue(String propertyName)
+    {
+		if ("deviceModel".equalsIgnoreCase(propertyName))
+		{
+			return propDeviceModel;
+		}
+		if ("enableExpressions".equalsIgnoreCase(propertyName))
+		{
+			return propEnableExpressions;
+		}
+		if ("displayGUI".equalsIgnoreCase(propertyName))
+		{
+			return propdisplayGUI;
+		}
+		if ("scoreBrowRaiserLeft".equalsIgnoreCase(propertyName))
+		{
+			return propScoreBrowRaiserLeft;
+		}
+		if ("scoreBrowRaiserRight".equalsIgnoreCase(propertyName))
+		{
+			return propScoreBrowRaiserRight;
+		}
+		if ("scoreBrowLowererLeft".equalsIgnoreCase(propertyName))
+		{
+			return propScoreBrowLowererLeft;
+		}
+		if ("scoreBrowLowererRight".equalsIgnoreCase(propertyName))
+		{
+			return propScoreBrowLowererRight;
+		}
+		if ("scoreSmile".equalsIgnoreCase(propertyName))
+		{
+			return propScoreSmile;
+		}
+		if ("scoreKiss".equalsIgnoreCase(propertyName))
+		{
+			return propScoreKiss;
+		}
+		if ("scoreMouthOpen".equalsIgnoreCase(propertyName))
+		{
+			return propScoreMouthOpen;
+		}
+		if ("scoreThongueOut".equalsIgnoreCase(propertyName))
+		{
+			return propScoreThongueOut;
+		}
+		if ("scoreEyesClosedLeft".equalsIgnoreCase(propertyName))
+		{
+			return propScoreEyesClosedLeft;
+		}
+		if ("scoreEyesClosedRight".equalsIgnoreCase(propertyName))
+		{
+			return propScoreEyesClosedRight;
+		}
+		if ("scoreEyesTurnLeft".equalsIgnoreCase(propertyName))
+		{
+			return propScoreEyesTurnLeft;
+		}
+		if ("scoreEyesTurnRight".equalsIgnoreCase(propertyName))
+		{
+			return propScoreEyesTurnRight;
+		}
+		if ("scoreEyesUp".equalsIgnoreCase(propertyName))
+		{
+			return propScoreEyesUp;
+		}
+		if ("scoreEyesDown".equalsIgnoreCase(propertyName))
+		{
+			return propScoreEyesDown;
+		}
+		if ("scorePuffLeft".equalsIgnoreCase(propertyName))
+		{
+			return propScorePuffLeft;
+		}
+		if ("scorePuffRight".equalsIgnoreCase(propertyName))
+		{
+			return propScorePuffRight;
+		}
+
+        return null;
+    }
+
+    /**
+     * sets a new value for the given property.
+     * @param propertyName   the name of the property
+     * @param newValue       the desired property value or null if not found
+     */
+    public Object setRuntimePropertyValue(String propertyName, Object newValue)
+    {
+		if ("deviceModel".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propDeviceModel;
+			propDeviceModel = Integer.parseInt((String)newValue);
+			return oldValue;
+		}
+		if ("enableExpressions".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propEnableExpressions;
+			if("true".equalsIgnoreCase((String)newValue))
+			{
+				propEnableExpressions = true;
+			}
+			else if("false".equalsIgnoreCase((String)newValue))
+			{
+				propEnableExpressions = false;
+			}
+			return oldValue;
+		}
+		if ("displayGUI".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propdisplayGUI;
+			if("true".equalsIgnoreCase((String)newValue))
+			{
+				propdisplayGUI = true;
+			}
+			else if("false".equalsIgnoreCase((String)newValue))
+			{
+				propdisplayGUI = false;
+			}
+			return oldValue;
+		}
+		if ("scoreBrowRaiserLeft".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreBrowRaiserLeft;
+			propScoreBrowRaiserLeft = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreBrowRaiserRight".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreBrowRaiserRight;
+			propScoreBrowRaiserRight = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreBrowLowererLeft".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreBrowLowererLeft;
+			propScoreBrowLowererLeft = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreBrowLowererRight".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreBrowLowererRight;
+			propScoreBrowLowererRight = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreSmile".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreSmile;
+			propScoreSmile = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreKiss".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreKiss;
+			propScoreKiss = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreMouthOpen".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreMouthOpen;
+			propScoreMouthOpen = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreThongueOut".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreThongueOut;
+			propScoreThongueOut = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreEyesClosedLeft".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreEyesClosedLeft;
+			propScoreEyesClosedLeft = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreEyesClosedRight".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreEyesClosedRight;
+			propScoreEyesClosedRight = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreEyesTurnLeft".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreEyesTurnLeft;
+			propScoreEyesTurnLeft = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreEyesTurnRight".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreEyesTurnRight;
+			propScoreEyesTurnRight = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreEyesUp".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreEyesUp;
+			propScoreEyesUp = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scoreEyesDown".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScoreEyesDown;
+			propScoreEyesDown = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scorePuffLeft".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScorePuffLeft;
+			propScorePuffLeft = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+		if ("scorePuffRight".equalsIgnoreCase(propertyName))
+		{
+			final Object oldValue = propScorePuffRight;
+			propScorePuffRight = Integer.parseInt(newValue.toString());
+			return oldValue;
+		}
+
+        return null;
+    }
+
+     /**
+      * Input Ports for receiving values.
+      */
+
+
+     /**
+      * Event Listerner Ports.
+      */
+
+	
+
+     /**
+      * called when model is started.
+      */
+      @Override
+      public void start()
+      {
+    	  AstericsErrorHandling.instance.getLogger().fine("\n\nIntel Real Sense selected model: " + propDeviceModel);
+    	  
+    	  
+    	  if(propEnableExpressions){
+    		  isExpressionON = 1;
+    	  }
+    	  else{
+    		  isExpressionON = 0;
+    	  }
+    	  
+    	  if(propdisplayGUI){
+    		  showDisplay = 1;
+    	  }
+    	  else{
+    		  showDisplay = 0;
+    	  }
+    	   	  
+    	  AstericsThreadPool.instance.execute(new Callable() {
+
+			@Override
+			public Object call() throws Exception {
+				if(bridge.init(deviceModels[propDeviceModel],isExpressionON,showDisplay)==-1){
+					throw new RuntimeException("Init of Intel Real Sense camera failed.");
+				}
+				AstericsErrorHandling.instance.getLogger().fine("Intel Real Sense camera initialized, starting tracking");
+				bridge.startTracking();
+				AstericsErrorHandling.instance.getLogger().fine("Intel Real Sense camera stopped tracking");
+
+				
+				return null;
+			}
+    		  
+		});
+      }
+
+     /**
+      * called when model is paused.
+      */
+      @Override
+      public void pause()
+      {
+    	  bridge.pause();
+          super.pause();
+      }
+
+     /**
+      * called when model is resumed.
+      */
+      @Override
+      public void resume()
+      {
+    	  bridge.resume();
+          super.resume();
+      }
+
+     /**
+      * called when model is stopped.
+      */
+      @Override
+      public void stop()
+      {
+    	  bridge.deactivate();
+          super.stop();
+      }
+}
