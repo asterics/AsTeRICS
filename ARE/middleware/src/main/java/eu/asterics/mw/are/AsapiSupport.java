f1bad78aff9cb4842e3a06787ff7055a6fa694f8
Benjamin Klaus <klues@live.de>
2016-12-23 12:47:46 +0100

    added new rest-API function to get currently deployed model name

--- ARE/middleware/src/main/java/eu/asterics/mw/are/AsapiSupport.java
+++ ARE/middleware/src/main/java/eu/asterics/mw/are/AsapiSupport.java
@@ -327,6 +327,19 @@ public class AsapiSupport
 		return modelToXML();
 	}
 
+  /**
+   * Returns the name of the currently deployed model
+   *
+   * @return the name of the currently deployed model or an empty string, if no model is deployed
+   */
+  public String getCurrentModelName() {
+    IRuntimeModel currentRuntimeModel  = DeploymentManager.instance.getCurrentRuntimeModel();
+    if(currentRuntimeModel == null) {
+      return "";
+    }
+    return currentRuntimeModel.getModelName();
+  }
+
 	/**
 	 * Returns a string encoding of the model defined in the filename given
 	 * as argument. If there is no model, an empty string is returned.
