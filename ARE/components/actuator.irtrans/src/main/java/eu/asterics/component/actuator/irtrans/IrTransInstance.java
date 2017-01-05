dfffd927057a393b069e926e4c98df1471a816cd
Benjamin Klaus <klues@live.de>
2017-01-05 11:22:33 +0100

    added missing imports

--- ARE/components/actuator.irtrans/src/main/java/eu/asterics/component/actuator/irtrans/IrTransInstance.java
+++ ARE/components/actuator.irtrans/src/main/java/eu/asterics/component/actuator/irtrans/IrTransInstance.java
@@ -25,6 +25,8 @@
 
 package eu.asterics.component.actuator.irtrans;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
