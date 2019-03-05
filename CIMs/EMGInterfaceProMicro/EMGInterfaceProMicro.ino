1eacbb196cfa277dc1f171fb7617e8b108d4e1f9
Benjamin Klaus <klaus@technikum-wien.at>
2019-03-05 11:45:11 +0100

    Revert "added mouse/keyboard functionality"
    This reverts commit 2dac05ad

--- CIMs/EMGInterfaceProMicro/EMGInterfaceProMicro.ino
+++ CIMs/EMGInterfaceProMicro/EMGInterfaceProMicro.ino
@@ -1,5 +1,3 @@
-#include <Keyboard.h>
-#include <Mouse.h>
 
 #define CHANNELS 4
 #define USE_BUFFERED_AVG 1
@@ -7,7 +5,6 @@
 #define STARTBUTTON 14
 #define DELAYTIME 50
 #define BLINKTIME 5
-#define MAX_INPUT_LENGTH 100
 
 
 int delta[CHANNELS] = {0};
@@ -29,8 +26,8 @@ float movAverage[CHANNELS] = {0};
 void setup() {
   // initialize the digital pin as an output.
   Serial.begin(9600);
-  //Serial.setTimeout(100);
 
+  TXLED0;
   pinMode(RXLED, OUTPUT);
   digitalWrite(RXLED, HIGH);
 
@@ -38,27 +35,19 @@ void setup() {
   pinMode(SENSORS[1], INPUT);
   pinMode(SENSORS[2], INPUT);
   pinMode(SENSORS[3], INPUT);
-  //pinMode(STARTBUTTON, INPUT_PULLUP);
-  Keyboard.begin();
-  Mouse.begin();
-  Mouse.screenSize(1920, 1080);
+  pinMode(STARTBUTTON, INPUT_PULLUP);
   delay(2);
 }
 
 int value;
 int cnt = 0;
-int runmode = 1;
-char receiveBuffer[MAX_INPUT_LENGTH + 1];
-char n1Buffer[20];
-char n2Buffer[20];
-int lastPosX = -1;
-int lastPosY = -1;
+int runmode = 0;
 
 // the loop routine runs over and over again forever:
 void loop() {
 
   // handle user button input
-  if (!runmode && digitalRead(STARTBUTTON) == LOW) {
+  if (digitalRead(STARTBUTTON) == LOW) {
     runmode = !runmode;
     delay(50);
     while (digitalRead(STARTBUTTON) == LOW);
@@ -66,45 +55,6 @@ void loop() {
   }
 
   if (runmode) {
-    if (Serial.available() > 0) {
-      Serial.readBytesUntil(';', receiveBuffer, MAX_INPUT_LENGTH);
-      //Serial.println("got: ");
-      //Serial.write(receiveBuffer, strlen(receiveBuffer));
-      int posX = 0, posY = 0;
-      int key1, key2, key3, mouseFunction;
-      int n = sscanf(receiveBuffer, "%s %s %d %d %d %d", &n1Buffer, &n2Buffer, &mouseFunction, &key1, &key2, &key3);
-      posX = round(atof(n1Buffer));
-      posY = round(atof(n2Buffer));
-      memset(receiveBuffer, 0, sizeof(receiveBuffer));
-      memset(n1Buffer, 0, sizeof(n1Buffer));
-      memset(n2Buffer, 0, sizeof(n2Buffer));
-      if (posX != lastPosX || posY != lastPosY) {
-        lastPosX = posX;
-        lastPosY = posY;
-        Mouse.moveTo(posX, posY);
-      }
-      if (mouseFunction != 0) {
-        switch (mouseFunction) {
-          case 1: Mouse.click(MOUSE_LEFT); break;
-          case 2: Mouse.click(MOUSE_LEFT); Mouse.click(MOUSE_LEFT); break;
-          case 3: Mouse.click(MOUSE_RIGHT); break;
-          case 4: Mouse.click(MOUSE_MIDDLE); break;
-          case 5: Mouse.press(MOUSE_LEFT); break;
-          case 6: Mouse.release(MOUSE_LEFT); break;
-        }
-      }
-      if (key1 != 0) {
-        Keyboard.press(key1);
-      }
-      if (key2 != 0) {
-        Keyboard.press(key2);
-      }
-      if (key3 != 0) {
-        Keyboard.press(key3);
-      }
-      Keyboard.releaseAll();
-    }
-
     // indicate send operation
     cnt = (cnt + 1) % BLINKTIME;
     if (!cnt)  digitalWrite(RXLED, !digitalRead(RXLED));
