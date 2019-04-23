ff982637a2aec6888d83e68a9634ab3694bafab7
Benjamin Klaus <klaus@technikum-wien.at>
2019-04-23 18:11:58 +0200

    added REST endpoint for current application version number

--- ARE/services/WebService/src/main/java/eu/asterics/mw/webservice/RestServer.java
+++ ARE/services/WebService/src/main/java/eu/asterics/mw/webservice/RestServer.java
@@ -1,26 +1,26 @@
 /*
  *    AsTeRICS - Assistive Technology Rapid Integration and Construction Set
- * 
- * 
- *        d8888      88888888888       8888888b.  8888888 .d8888b.   .d8888b. 
+ *
+ *
+ *        d8888      88888888888       8888888b.  8888888 .d8888b.   .d8888b.
  *       d88888          888           888   Y88b   888  d88P  Y88b d88P  Y88b
- *      d88P888          888           888    888   888  888    888 Y88b.     
- *     d88P 888 .d8888b  888   .d88b.  888   d88P   888  888         "Y888b.  
+ *      d88P888          888           888    888   888  888    888 Y88b.
+ *     d88P 888 .d8888b  888   .d88b.  888   d88P   888  888         "Y888b.
  *    d88P  888 88K      888  d8P  Y8b 8888888P"    888  888            "Y88b.
  *   d88P   888 "Y8888b. 888  88888888 888 T88b     888  888    888       "888
  *  d8888888888      X88 888  Y8b.     888  T88b    888  Y88b  d88P Y88b  d88P
- * d88P     888  88888P' 888   "Y8888  888   T88b 8888888 "Y8888P"   "Y8888P" 
+ * d88P     888  88888P' 888   "Y8888  888   T88b 8888888 "Y8888P"   "Y8888P"
  *
  *
- *                    homepage: http://www.asterics.org 
+ *                    homepage: http://www.asterics.org
  *
- *         This project has been funded by the European Commission, 
+ *         This project has been funded by the European Commission,
  *                      Grant Agreement Number 247730
- *  
- *  
+ *
+ *
  *         Dual License: MIT or GPL v3.0 with "CLASSPATH" exception
  *         (please refer to the folder LICENSE)
- * 
+ *
  */
 
 package eu.asterics.mw.webservice;
@@ -76,6 +76,14 @@ public class RestServer {
     private AsapiSupport asapiSupport = new AsapiSupport();
     private Logger logger = AstericsErrorHandling.instance.getLogger();
     private AstericsAPIEncoding astericsAPIEncoding = new AstericsAPIEncoding();
+
+    @Path("/version")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Description("Returns the current ARE version")
+    public String getVersion() {
+        return "#{APPLICATION_VERSION_NUMBER}#";
+    }
 
     @Path("/restfunctions")
     @GET
