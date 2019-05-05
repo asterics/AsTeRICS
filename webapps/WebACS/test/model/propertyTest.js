cbeea88976cd955a497e57ccfec866fd957d4e2d
Martin Deinhofer <deinhofe@technikum-wien.at>
2019-05-05 23:28:50 +0200

    removed old WebACS Now only the the link webacs.asterics.eu is valid

--- webapps/WebACS/test/model/propertyTest.js
+++
@@ -1,8 +0,0 @@
-QUnit.module( 'property' );
-
-QUnit.test( 'property initialization', function( assert ) {
-	var ppt = ACS.property('keyString', 3, '2.5');
-	assert.strictEqual(ppt.getKey(), 'keyString');
-	assert.strictEqual(ppt.getType(), 3);
-	assert.strictEqual(ppt.value, '2.5');
-});
