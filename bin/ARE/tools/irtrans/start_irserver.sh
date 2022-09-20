d17a2198637eb1628805362406cdb7087e0fdbe6
Martin Deinhofer <deinhofer.m@gmail.com>
2022-09-20 11:27:10 +0200

    Revert "fixed hardcoded absoulte path of irserver cmds, use dirname instead"
    This reverts commit 3d9bbf4f6f2f17c07f6428dde1e9d6f176c9a20b.

--- bin/ARE/tools/irtrans/start_irserver.sh
+++ bin/ARE/tools/irtrans/start_irserver.sh
@@ -5,9 +5,6 @@ sudo pkill irserver
 sudo fuser -k /dev/ttyIRTrans
 
 DEV_NAME=$(readlink /dev/ttyIRTrans)
-BIN_PATH=$(dirname $0)
-echo "using BIN_PATH="$BIN_PATH
-
 if [ -z "$DEV_NAME" ]; then
   echo "did not find link 'ttyIRTrans', using fallback 'ttyUSB0'."
   DEV_NAME="ttyUSB0"
@@ -16,11 +13,11 @@ else
 fi
 
 if [ "$(dpkg --print-architecture)" == "amd64" ]; then
-  sudo $BIN_PATH/irserver64 /dev/$DEV_NAME
+  sudo /opt/asterics-ergo/app/tools/irserver64 /dev/$DEV_NAME
 elif [ "$(dpkg --print-architecture)" == "armhf" ]; then
-  sudo $BIN_PATH/irserver_arm /dev/$DEV_NAME
+  sudo /opt/asterics-ergo/app/tools/irserver_arm /dev/$DEV_NAME
 else
-  sudo $BIN_PATH/irserver /dev/$DEV_NAME
+  sudo /opt/asterics-ergo/app/tools/irserver /dev/$DEV_NAME
 fi
 echo "press any key to close..."
 read -n 1 -s
